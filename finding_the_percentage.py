dbdaf55292020331891f995fb083711a7c0ca635
AchalNarwade <achalnarwade211@gmail.com>
2025-08-13 15:33:07 +0530

    commit

---
+++ finding_the_percentage.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/finding-the-percentage/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores#kay value pairs generation 
+    query_name = input()
+    avg=sum(student_marks[query_name])/len(student_marks[query_name])
+    print(f"{avg:.2f}")
